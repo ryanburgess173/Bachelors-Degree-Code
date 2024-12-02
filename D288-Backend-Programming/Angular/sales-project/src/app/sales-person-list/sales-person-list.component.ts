5cfe4f15ebd9c8a08330da124fc6073ca0f602a1
Johnathan Burgess <ryanburgess173@outlook.com>
2024-12-02 18:41:32 -0500

    Section 8 of udemy course for D288

--- D288-Backend-Programming/Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ D288-Backend-Programming/Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -5,15 +5,15 @@ import { SalesPerson } from './sales-person';
   selector: 'app-sales-person-list',
   standalone: false,
   
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrl: './sales-person-list.component.css'
 })
 export class SalesPersonListComponent {
 
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("John", "Doe", "john.doe@burgesscompany.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@burgesscompany.com", 70000),
     new SalesPerson("Bob", "Jenkins", "bob.jenkins@burgesscompany.com", 60000),
-    new SalesPerson("Henry", "Wrangler", "henry.wrangler@burgesscompany.com", 55000),
+    new SalesPerson("Henry", "Wrangler", "henry.wrangler@burgesscompany.com", 65000),
     new SalesPerson("Travis", "American", "travis.american@burgesscompany.com", 40000)
   ];
 
