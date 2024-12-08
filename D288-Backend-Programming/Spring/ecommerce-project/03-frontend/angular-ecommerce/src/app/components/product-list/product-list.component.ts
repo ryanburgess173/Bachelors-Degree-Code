ba69c04bea2d123289925aec89cd7cff4a9bac62
Johnathan Burgess <ryanburgess173@outlook.com>
2024-12-08 11:18:25 -0500

    more changes during section 11

--- D288-Backend-Programming/Spring/ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ D288-Backend-Programming/Spring/ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { Product } from '../../common/product';
 @Component({
   selector: 'app-product-list',
   standalone: false,
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
